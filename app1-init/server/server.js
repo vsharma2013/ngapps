47d32a897499cd91a070c2788c2dc685037a6423
Vishal Sharma <vishal2033@gmail.com>
2016-05-10 19:45:08 +0530

    basi client server

---
+++ app1-init/server/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let koa = require('koa');
+let app = koa();
+let ks = require('koa-static');
+
+let client = __dirname.replace('server', 'client');
+let bower = __dirname.replace('app1-init/server', 'bower_components');
+console.log(bower)
+app.use(ks(client, {}));
+app.use(ks(bower, {}));
+
+app.listen(9999);	
+
+console.log('server started at port 9999')
