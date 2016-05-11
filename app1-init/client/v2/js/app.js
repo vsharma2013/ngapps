8595a6b8ce1ef66ea08f2a250884246ef512a35c
Vishal Sharma <vishal2033@gmail.com>
2016-05-11 17:27:03 +0530

    v2 controller as usage

---
+++ app1-init/client/v2/js/app.js
@@ -0,0 +1,29 @@
+(function(){
+	
+	function appRoutes($routeProvider){
+		$routeProvider
+			.when('/docs', docListRoute)
+			.when('/docs/:id', docDetailRoute)
+			.otherwise({redirectTo : '/docs'})
+	}
+
+	var appRouteConfig = ['$routeProvider', appRoutes];
+	
+	angular
+		.module('documentApp', ['ngRoute', 'documentControllers'])
+		.config(appRouteConfig);
+	
+
+	var docListRoute = {
+		templateUrl : 'partials/doc-list.html',
+		controller : 'docListController',
+		controllerAs : 'vm'
+	};
+
+	var docDetailRoute = {
+		templateUrl : 'partials/doc-detail.html',
+		controller : 'docDetailController',
+		controllerAs : 'vm'
+
+	};
+})();
---
+++ app1-init/client/v2/js/controllers.js
@@ -0,0 +1,32 @@
+(function(){
+	angular
+	.module('documentControllers', [])
+	.controller('docListController', docListController)
+	.controller('docDetailController', docDetailController)
+
+	function docListController($scope, $http){
+		var vm = this
+		$http.get('/api/doc').success(function(data){
+			vm.docs = data.result;
+		});
+		vm.orderProp = 'name';
+	}
+	docListController.$inject = ['$scope', '$http'];
+
+	function docDetailController($scope, $http, $routeParams){
+		var vm = this
+		var url = '/api/doc/' + $routeParams.id;
+
+		$http.get(url).success(function(data){
+			vm.doc = data.result;
+		});
+
+		$scope.saveDoc = function(){
+			var saveDoc = angular.toJson(vm.doc);
+			$http.put('/api/doc', saveDoc)
+			     .success(function (res) { alert('Save status : ' + res.success) })
+			     .error(function(err) { console.log(arguments); alert ('Failed to save the document.')})
+		}
+	}
+	docDetailController.$inject = ['$scope', '$http', '$routeParams'];
+})();
