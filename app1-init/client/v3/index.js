be424d2d212e8cb46de566569ad5ea0a83464f86
Vishal Sharma <vishal2033@gmail.com>
2016-05-14 10:39:59 +0530

    v3 tabs directive

--- app1-init/client/v3/index.js
+++ app1-init/client/v3/index.js
@@ -7,6 +7,8 @@
 		.directive('myCustomer3', function() { return MyCustomer3; })
 		.directive('myCurrentTime', MyCurrentTime)
 		.directive('myDialog', function() { return MyDialog;})
+		.directive('myTabs', function() { return MyTabs;} )
+		.directive('myPane', function() { return MyPane;})
 
 	function Controller($scope){
 		$scope.name = 'Vishal Sharma';
@@ -15,6 +17,8 @@
 		$scope.naomi = { name : 'Naomi', address : 'Naomi address'};
 
 		$scope.format = 'M/d/yy h:mm:ss a';
+
+		
 	}
 	Controller.$inject = ['$scope'];
 
@@ -61,5 +65,36 @@
 		scope : {},
 		transclude : true,
 		templateUrl : 'my-dialog.html'
+	};
+
+	var MyTabs = {
+		restrict : 'E',
+		templateUrl : 'my-tabs.html',
+		transclude : true,
+		scope : {},
+		controller : ['$scope', function($scope){
+			var panes = $scope.panes = [];
+
+			$scope.select = function(pane){
+				angular.forEach(panes, function(p){ p.selected = false});
+				pane.selected = true;
+			} 
+
+			this.addPane = function(pane){
+				if(panes.length === 0)
+					$scope.select(pane);
+
+				panes.push(pane);
+			}
+		}]
+	};
+
+	var MyPane = {
+		require: '^^myTabs',
+		restrict : 'E',
+		templateUrl : 'my-pane.html',
+		transclude : true,
+		scope : { title : '@'},
+		link : function(scope, elem, attrs, tabsCtrl) { tabsCtrl.addPane(scope);}
 	};
 })();
