c8b2c08d85798668eed56e26503c928c8e8edf75
Vishal Sharma <vishal2033@gmail.com>
2016-05-16 15:06:11 +0530

    v5 init with react

--- app1-init/client/v5/index.js
+++ app1-init/client/v5/index.js
@@ -1,14 +1,23 @@
+var commentList_tpl = <div className='commentList'>Hello world, I am a comment list.</div>;
+var commentForm_tpl = <div className='commentForm'>Hello world, I am a comment form.</div>
+				
+			
+var CommentListClass = { render : function(){ return commentList_tpl;} };
+var CommentFormClass = { render : function(){ return commentForm_tpl; } };
 
-var CommentBoxClass = {
-	render : function(){
-		return (
-			<div className='commentBox'>
-				Hello world, I am a comment box.
-			</div>
-		);
-	}
-};
+var CommentForm = React.createClass(CommentFormClass);
+var CommentList = React.createClass(CommentListClass);
 
+var commnetBox_tpl = <div className='commentBox'>
+						<h2>Comments</h2>
+						<CommentList />
+						<CommentForm />
+					 </div>;
+
+var CommentBoxClass = { render : function(){ return commnetBox_tpl ; }};
 var CommentBox = React.createClass(CommentBoxClass);
 
-ReactDOM.render(<CommentBox/>, document.getElementById('content'));
+ReactDOM.render(
+	<CommentBox/>, 
+	document.getElementById('content')
+);
